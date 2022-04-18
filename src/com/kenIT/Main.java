b5dfa25f0031fa0839fa468321ec029c4bd3c785
- <=>
2022-04-18 16:41:41 +0700

    fix toCharArray

--- src/com/kenIT/Main.java
+++ src/com/kenIT/Main.java
@@ -215,11 +215,11 @@ public class Main {
 
         //toCharArray method
         //Phương thức toCharArray() được sử dụng để chuyển đổi chuỗi thành các mảng ký tự. Nó trả về một mảng ký từ có độ dài tương đương độ dài của chuỗi.
-        String s1 = "hello";
-        char[] ch = s1.toCharArray();
-        for (int i = 0; i < ch.length; i++) {
-            System.out.println(ch[i]);
-        }
+//        String s1 = "hello";
+//        char[] ch = s1.toCharArray();
+//        for (int i = 0; i < ch.length; i++) {
+//            System.out.println(ch[i]);
+//        }
     }
 }
 
