c98d5bba600d994c3170d20c2ce310ce24014495
- <=>
2022-04-18 16:40:54 +0700

    toCharArray method

--- src/com/kenIT/Main.java
+++ src/com/kenIT/Main.java
@@ -212,6 +212,14 @@ public class Main {
 //        String s1 = "Welcome to Java";
 //        System.out.println(s1.substring(0,2));
 //        System.out.println(s1.substring(0));
+
+        //toCharArray method
+        //Phương thức toCharArray() được sử dụng để chuyển đổi chuỗi thành các mảng ký tự. Nó trả về một mảng ký từ có độ dài tương đương độ dài của chuỗi.
+        String s1 = "hello";
+        char[] ch = s1.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            System.out.println(ch[i]);
+        }
     }
 }
 
