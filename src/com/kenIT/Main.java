0162381566709cfb596b8d7c98f388786f07e6fa
- <=>
2022-04-18 14:27:44 +0700

    fix intern

--- src/com/kenIT/Main.java
+++ src/com/kenIT/Main.java
@@ -127,11 +127,11 @@ public class Main {
 
         //intern method
         //Phương thức intern() có thể được sử dụng để trả về chuỗi từ Pool chứa hằng số chuỗi khi nó được tạo bởi từ khóa new.
-        String s1 = new String("hello");
-        String s2 = "hello";
-        String s3 = s1.intern();
-        System.out.println(s1 == s2);
-        System.out.println(s2 == s3);
+//        String s1 = new String("hello");
+//        String s2 = "hello";
+//        String s3 = s1.intern();
+//        System.out.println(s1 == s2);
+//        System.out.println(s2 == s3);
 
     }
 }
