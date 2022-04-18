a48be926b4aa4aeba49fc044d890ed32d07f0c88
- <=>
2022-04-18 16:32:32 +0700

    startsWith mehod

--- src/com/kenIT/Main.java
+++ src/com/kenIT/Main.java
@@ -197,6 +197,14 @@ public class Main {
 //        for (String w : s1.split("\\s", 2)) {
 //            System.out.println(w);
 //        }
+
+        //startsWith method
+        //Phương thức startsWith() được sử dụng để kiểm tra tiền tố của chuỗi có khớp với giá trị tiền tố đã nhập không, nếu đúng trả về true, sai trả về false.
+        String s1 = "Welcome to Java";
+        System.out.println(s1.startsWith("W"));
+        System.out.println(s1.startsWith("Welcome"));
+        System.out.println(s1.startsWith("Welcome",0));
+
     }
 }
 
